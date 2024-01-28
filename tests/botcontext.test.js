14fa89b6e8dc3b0b800241847c528c62a1f62d96
Evan Prodromou <evan@prodromou.name>
2024-01-28 13:19:16 -0500

    feat: send a note
    Signed-off-by: Evan Prodromou <evan@prodromou.name>

--- tests/botcontext.test.js
+++ tests/botcontext.test.js
@@ -1,4 +1,4 @@
-import { describe, it, before, after } from 'node:test'
+import { describe, it, before, after, beforeEach } from 'node:test'
 import assert from 'node:assert'
 import { BotContext } from '../lib/botcontext.js'
 import { Sequelize } from 'sequelize'
@@ -14,17 +14,6 @@ import { promisify } from 'node:util'
 import nock from 'nock'
 
 const as2import = promisify(as2.import)
-const as2export = (obj) => {
-  return new Promise((resolve, reject) => {
-    obj.export((err, doc) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(doc)
-      }
-    })
-  })
-}
 const as2write = (obj) => {
   return new Promise((resolve, reject) => {
     obj.write((err, doc) => {
@@ -47,7 +36,9 @@ describe('BotContext', () => {
   let client = null
   let distributor = null
   let context = null
+  let postInbox2 = 0
   before(async () => {
+    formatter = new UrlFormatter('https://botsrodeo.example')
     connection = new Sequelize('sqlite::memory:', { logging: false })
     await connection.authenticate()
     botDataStorage = new BotDataStorage(connection)
@@ -56,9 +47,8 @@ describe('BotContext', () => {
     await objectStorage.initialize()
     keyStorage = new KeyStorage(connection)
     await keyStorage.initialize()
-    actorStorage = new ActorStorage(connection)
+    actorStorage = new ActorStorage(connection, formatter)
     await actorStorage.initialize()
-    formatter = new UrlFormatter('https://botsrodeo.example')
     client = new ActivityPubClient(keyStorage, formatter)
     distributor = new ActivityDistributor(client, formatter, actorStorage)
     await objectStorage.create(await as2import({
@@ -67,15 +57,35 @@ describe('BotContext', () => {
       attributedTo: formatter.format({ username: 'test1' }),
       to: 'https://www.w3.org/ns/activitystreams#Public'
     }))
+    const actor2 = await as2import({
+      id: 'https://social.example/user/test2',
+      type: 'Person',
+      preferredUsername: 'test2',
+      inbox: 'https://social.example/user/test2/inbox',
+      outbox: 'https://social.example/user/test2/outbox',
+      followers: 'https://social.example/user/test2/followers',
+      following: 'https://social.example/user/test2/following',
+      liked: 'https://social.example/user/test2/liked'
+    })
+    await actorStorage.addToCollection('test1', 'followers', actor2)
+    const actor2Text = await as2write(actor2)
     const object = await as2import({
-      id: 'https://social.example/users/test2/object/1',
+      id: 'https://social.example/user/test2/object/1',
       type: 'Object',
-      attributedTo: 'https://social.example/users/test2',
+      attributedTo: 'https://social.example/user/test2',
       to: 'https://www.w3.org/ns/activitystreams#Public'
     })
     const objectText = await as2write(object)
     nock('https://social.example')
-      .get('/users/test2/object/1')
+      .get('/user/test2')
+      .reply(200, actor2Text, { 'Content-Type': 'application/activity+json' })
+      .persist()
+      .post('/user/test2/inbox')
+      .reply((uri, requestBody) => {
+        postInbox2 += 1
+        return [202, 'accepted']
+      })
+      .get('/user/test2/object/1')
       .reply(200, objectText, { 'Content-Type': 'application/activity+json' })
       .persist()
   })
@@ -90,6 +100,9 @@ describe('BotContext', () => {
     botDataStorage = null
     objectStorage = null
     connection = null
+  })
+  beforeEach(async () => {
+    postInbox2 = 0
   })
   it('can initialize', async () => {
     context = new BotContext(
@@ -123,10 +136,21 @@ describe('BotContext', () => {
     assert.strictEqual(object.type, 'https://www.w3.org/ns/activitystreams#Object')
   })
   it('can get a remote object', async () => {
-    const id = 'https://social.example/users/test2/object/1'
+    const id = 'https://social.example/user/test2/object/1'
     const object = await context.getObject(id)
     assert.ok(object)
     assert.strictEqual(object.id, id)
     assert.strictEqual(object.type, 'https://www.w3.org/ns/activitystreams#Object')
+  })
+  it('can send a note', async () => {
+    const content = 'Hello World'
+    const to = 'https://www.w3.org/ns/activitystreams#Public'
+    const note = await context.sendNote(content, { to })
+    assert.ok(note)
+    assert.strictEqual(postInbox2, 1)
+    const outbox = await actorStorage.getCollection('test1', 'outbox')
+    assert.strictEqual(outbox.totalItems, 1)
+    const inbox = await actorStorage.getCollection('test1', 'inbox')
+    assert.strictEqual(inbox.totalItems, 1)
   })
 })
