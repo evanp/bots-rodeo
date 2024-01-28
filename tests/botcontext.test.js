afcd849a64a256452b50ee3735ed6a98f6494490
Evan Prodromou <evan@prodromou.name>
2024-01-28 12:37:04 -0500

    feat: add constructor arguments to BotContext
    Signed-off-by: Evan Prodromou <evan@prodromou.name>

--- tests/botcontext.test.js
+++ tests/botcontext.test.js
@@ -4,11 +4,21 @@ import { BotContext } from '../lib/botcontext.js'
 import { Sequelize } from 'sequelize'
 import { BotDataStorage } from '../lib/botdatastorage.js'
 import { ObjectStorage } from '../lib/objectstorage.js'
+import { KeyStorage } from '../lib/keystorage.js'
+import { UrlFormatter } from '../lib/urlformatter.js'
+import { ActivityPubClient } from '../lib/activitypubclient.js'
+import { ActivityDistributor } from '../lib/activitydistributor.js'
+import { ActorStorage } from '../lib/actorstorage.js'
 
 describe('BotContext', () => {
   let connection = null
   let botDataStorage = null
   let objectStorage = null
+  let keyStorage = null
+  let actorStorage = null
+  let formatter = null
+  let client = null
+  let distributor = null
   let context = null
   before(async () => {
     connection = new Sequelize('sqlite::memory:', { logging: false })
@@ -17,16 +27,38 @@ describe('BotContext', () => {
     await botDataStorage.initialize()
     objectStorage = new ObjectStorage(connection)
     await objectStorage.initialize()
+    keyStorage = new KeyStorage(connection)
+    await keyStorage.initialize()
+    actorStorage = new ActorStorage(connection)
+    await actorStorage.initialize()
+    formatter = new UrlFormatter('https://botsrodeo.example')
+    client = new ActivityPubClient(keyStorage, formatter)
+    distributor = new ActivityDistributor(client, formatter, actorStorage)
   })
   after(async () => {
     await connection.close()
     context = null
+    distributor = null
+    client = null
+    formatter = null
+    actorStorage = null
+    keyStorage = null
     botDataStorage = null
     objectStorage = null
     connection = null
   })
   it('can initialize', async () => {
-    context = new BotContext('test1', botDataStorage, objectStorage)
+    context = new BotContext(
+      'test1',
+      botDataStorage,
+      objectStorage,
+      actorStorage,
+      client,
+      distributor
+    )
+  })
+  it('can get the bot ID', () => {
+    assert.strictEqual(context.botId, 'test1')
   })
   it('can set a value', async () => {
     await context.setData('key1', 'value1')
