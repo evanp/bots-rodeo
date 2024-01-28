e92d5960e9f7cb336c9b74146680bd9e548648ca
Evan Prodromou <evan@prodromou.name>
2024-01-28 12:56:40 -0500

    feat: BotContext can get an object
    Signed-off-by: Evan Prodromou <evan@prodromou.name>

--- tests/botcontext.test.js
+++ tests/botcontext.test.js
@@ -9,6 +9,33 @@ import { UrlFormatter } from '../lib/urlformatter.js'
 import { ActivityPubClient } from '../lib/activitypubclient.js'
 import { ActivityDistributor } from '../lib/activitydistributor.js'
 import { ActorStorage } from '../lib/actorstorage.js'
+import as2 from 'activitystrea.ms'
+import { promisify } from 'node:util'
+import nock from 'nock'
+
+const as2import = promisify(as2.import)
+const as2export = (obj) => {
+  return new Promise((resolve, reject) => {
+    obj.export((err, doc) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(doc)
+      }
+    })
+  })
+}
+const as2write = (obj) => {
+  return new Promise((resolve, reject) => {
+    obj.write((err, doc) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(doc)
+      }
+    })
+  })
+}
 
 describe('BotContext', () => {
   let connection = null
@@ -34,6 +61,23 @@ describe('BotContext', () => {
     formatter = new UrlFormatter('https://botsrodeo.example')
     client = new ActivityPubClient(keyStorage, formatter)
     distributor = new ActivityDistributor(client, formatter, actorStorage)
+    await objectStorage.create(await as2import({
+      id: formatter.format({ username: 'test1', type: 'object', nanoid: '_pEWsKke-7lACTdM3J_qd' }),
+      type: 'Object',
+      attributedTo: formatter.format({ username: 'test1' }),
+      to: 'https://www.w3.org/ns/activitystreams#Public'
+    }))
+    const object = await as2import({
+      id: 'https://social.example/users/test2/object/1',
+      type: 'Object',
+      attributedTo: 'https://social.example/users/test2',
+      to: 'https://www.w3.org/ns/activitystreams#Public'
+    })
+    const objectText = await as2write(object)
+    nock('https://social.example')
+      .get('/users/test2/object/1')
+      .reply(200, objectText, { 'Content-Type': 'application/activity+json' })
+      .persist()
   })
   after(async () => {
     await connection.close()
@@ -54,7 +98,8 @@ describe('BotContext', () => {
       objectStorage,
       actorStorage,
       client,
-      distributor
+      distributor,
+      formatter
     )
   })
   it('can get the bot ID', () => {
@@ -69,5 +114,19 @@ describe('BotContext', () => {
   })
   it('can delete a value', async () => {
     await context.deleteData('key1')
+  })
+  it('can get a local object', async () => {
+    const id = formatter.format({ username: 'test1', type: 'object', nanoid: '_pEWsKke-7lACTdM3J_qd' })
+    const object = await context.getObject(id)
+    assert.ok(object)
+    assert.strictEqual(object.id, id)
+    assert.strictEqual(object.type, 'https://www.w3.org/ns/activitystreams#Object')
+  })
+  it('can get a remote object', async () => {
+    const id = 'https://social.example/users/test2/object/1'
+    const object = await context.getObject(id)
+    assert.ok(object)
+    assert.strictEqual(object.id, id)
+    assert.strictEqual(object.type, 'https://www.w3.org/ns/activitystreams#Object')
   })
 })
