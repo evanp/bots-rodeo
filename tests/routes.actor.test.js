c791a2ea415d5a2741b377a4982c442ec2fcb86d
Evan Prodromou <evan@prodromou.name>
2024-09-07 19:50:12 -0400

    feat: show bot name and desc; closes #4
    Signed-off-by: Evan Prodromou <evan@prodromou.name>

--- tests/routes.actor.test.js
+++ tests/routes.actor.test.js
@@ -29,6 +29,51 @@ describe('actor routes', async () => {
     it('should return an object with an id matching the request', async () => {
       assert.strictEqual(response.body.id, origin + '/user/ok')
     })
+    it('should return an object with a type', async () => {
+      assert.strictEqual(typeof response.body.type, 'string')
+    })
+    it('should return an object with a type matching the request', async () => {
+      assert.strictEqual(response.body.type, 'Service')
+    })
+    it('should return an object with a preferredUsername', async () => {
+      assert.strictEqual(typeof response.body.preferredUsername, 'string')
+    })
+    it('should return an object with a preferredUsername matching the request', async () => {
+      assert.strictEqual(response.body.preferredUsername, 'ok')
+    })
+    it('should return an object with an inbox', async () => {
+      assert.strictEqual(typeof response.body.inbox, 'string')
+    })
+    it('should return an object with an outbox', async () => {
+      assert.strictEqual(typeof response.body.outbox, 'string')
+    })
+    it('should return an object with a followers', async () => {
+      assert.strictEqual(typeof response.body.followers, 'string')
+    })
+    it('should return an object with a following', async () => {
+      assert.strictEqual(typeof response.body.following, 'string')
+    })
+    it('should return an object with a liked', async () => {
+      assert.strictEqual(typeof response.body.liked, 'string')
+    })
+    it('should return an object with a to', async () => {
+      assert.strictEqual(typeof response.body.to, 'string')
+    })
+    it('should return an object with a to matching the request', async () => {
+      assert.strictEqual(response.body.to, 'as:Public')
+    })
+    it('should return an object with a summary', async () => {
+      assert.strictEqual(typeof response.body.summary, 'string')
+    })
+    it('should return an object with a summary matching the request', async () => {
+      assert.strictEqual(response.body.summary, 'A bot that says "OK" when mentioned.')
+    })
+    it('should return an object with a name', async () => {
+      assert.strictEqual(typeof response.body.name, 'string')
+    })
+    it('should return an object with a name matching the request', async () => {
+      assert.strictEqual(response.body.name, 'OK Bot')
+    })
   })
 
   describe('GET non-existent user', async () => {
