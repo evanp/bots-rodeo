f7a64f9db5213b509d7485fbe00a0979962163f1
Evan Prodromou <evan@prodromou.name>
2024-09-07 16:01:55 -0400

    test: actor routes
    Signed-off-by: Evan Prodromou <evan@prodromou.name>

--- tests/routes.actor.test.js
+++ tests/routes.actor.test.js
@@ -30,4 +30,44 @@ describe('actor routes', async () => {
       assert.strictEqual(response.body.id, origin + '/user/ok')
     })
   })
+
+  describe('GET non-existent user', async () => {
+    let response = null
+    it('should work without an error', async () => {
+      response = await request(app).get('/user/dne')
+    })
+    it('should return 404 Not Found', async () => {
+      assert.strictEqual(response.status, 404)
+    })
+    it('should return Problem Details JSON', async () => {
+      assert.strictEqual(response.type, 'application/problem+json')
+    })
+    it('should return an object', async () => {
+      assert.strictEqual(typeof response.body, 'object')
+    })
+    it('should return an object with a type', async () => {
+      assert.strictEqual(typeof response.body.type, 'string')
+    })
+    it('should return an object with an type matching the request', async () => {
+      assert.strictEqual(response.body.type, 'about:blank')
+    })
+    it('should return an object with a title', async () => {
+      assert.strictEqual(typeof response.body.title, 'string')
+    })
+    it('should return an object with a title matching the request', async () => {
+      assert.strictEqual(response.body.title, 'Not Found')
+    })
+    it('should return an object with a status', async () => {
+      assert.strictEqual(typeof response.body.status, 'number')
+    })
+    it('should return an object with a status matching the request', async () => {
+      assert.strictEqual(response.body.status, 404)
+    })
+    it('should return an object with a detail', async () => {
+      assert.strictEqual(typeof response.body.detail, 'string')
+    })
+    it('should return an object with a detail matching the request', async () => {
+      assert.strictEqual(response.body.detail, 'User dne not found')
+    })
+  })
 })
